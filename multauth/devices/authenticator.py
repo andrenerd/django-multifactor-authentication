ae64a8f59358092ac0cd7336ca6bd3212ec6eaf5
Andrenerd <andreid.31415@gmail.com>
2020-12-05 05:10:17 +0100

    update authenticator device

--- multauth/devices/authenticator.py
+++ multauth/devices/authenticator.py
@@ -6,37 +6,52 @@ from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 from django_otp.util import random_hex
+from django_otp.plugins.otp_totp.models import TOTPDevice
 
 from .abstract import AbstractDevice, AbstractUserMixin
 
 
-try:
-    AuthenticatorProviderPath = settings.MULTAUTH_DEVICE_AUTHENTICATOR_PROVIDER
-    AuthenticatorProvider = import_string(AuthenticatorProviderPath) # ex. multauth.providers.GauthenticatorProvider
-except AttributeError:
-    from ..providers.mail import GauthenticatorProvider
-    AuthenticatorProvider = GauthenticatorProvider
+# reserved 
+# try:
+#     AuthenticatorProviderPath = settings.MULTAUTH_DEVICE_AUTHENTICATOR_PROVIDER
+#     AuthenticatorProvider = import_string(AuthenticatorProviderPath) # ex. multauth.providers.GauthenticatorProvider
+# except AttributeError:
+#     from ..providers.mail import GauthenticatorProvider
+#     AuthenticatorProvider = GauthenticatorProvider
 
 
 DEBUG = getattr(settings, 'DEBUG', False)
 MULTAUTH_DEBUG = getattr(settings, 'MULTAUTH_DEBUG', DEBUG)
 
 
-class AuthenticatorDevice(AbstractDevice):
+class AuthenticatorDevice(AbstractDevice, TOTPDevice):
+    token = None
+    valid_until = None
+
     USER_MIXIN = 'AuthenticatorUserMixin'
 
     def __eq__(self, other):
         if not isinstance(other, AuthenticatorDevice):
             return False
 
-        return self.authenticator == other.authenticator \
-            and self.key == other.key
+        return self.key == other.key
 
     def __hash__(self):
         return hash((self.authenticator,))
 
     def clean(self):
         super().clean()
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super().save(*args, **kwargs)
+
+    # def generate_key(self):
+    #     return 'AAAABBBBCCCCDDDD'
+
+    def generate_token(self):
+        return None
 
     def generate_challenge(self, request=None):
         self.generate_token()
@@ -59,7 +74,12 @@ class AuthenticatorUserMixin(AbstractUserMixin):
         return True
 
     def get_authenticator_device(self):
-        return None
+        try:
+            device = AuthenticatorDevice.objects.get(user=self)
+        except AuthenticatorDevice.DoesNotExist:
+            device = None
+
+        return device
 
     def verify(self, request=None):
         super().verify(request)
