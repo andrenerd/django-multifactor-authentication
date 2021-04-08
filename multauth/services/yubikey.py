84b0766417fadf8a8143405ac4e711d9e541836d
Andrenerd <andreid.31415@gmail.com>
2021-04-08 05:08:09 +0200

    prototype yubikey service

--- multauth/services/yubikey.py
+++ multauth/services/yubikey.py
@@ -63,6 +63,7 @@ def key_validator(value):
 class YubikeyService(ThrottlingMixin, PasscodeServiceMixin, AbstractService):
     private_id = models.CharField(max_length=MULTAUTH_PRIVATE_ID_LENGTH * 2, validators=[private_id_validator], default=private_id_generator)
     key = models.CharField(max_length=32, validators=[key_validator], default=key_generator)
+    session = models.PositiveIntegerField(default=0)
 
     # see django_otp.plugins.otp_totp.models.TOTPService
     # key = models.CharField(max_length=80, validators=[key_validator], default=key_generator) # a hex-encoded secret key of up to 40 bytes
